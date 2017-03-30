1968f8896185f64eb69ae3fb005fee7630f9aea4
jpnsingh <jpnsingh86@gmail.com>
2017-03-30 17:59:56 +0530

    Syncing today's changes so far from Vipin;

---
+++ liveon/views/dashboard/js/dashboard.controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    angular
+    .module('olh-liveon-dashboard')
+    .controller('DashboardController', DashboardController);
+
+    DashboardController.$inject = ['DashboardService'];
+    function DashboardController(DashboardService) {
+        var vm = this;
+
+        vm.remindMeOptions = DashboardService.getRemindMeOptions();
+        vm.yesNoOptions = DashboardService.getYesNoOptions();
+    }
+})();
+
---
+++ liveon/views/dashboard/js/dashboard.service.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    angular
+    .module('olh-liveon-dashboard')
+    .factory('DashboardService', DashboardService);
+
+    DashboardService.$inject = [];
+    function DashboardService() {
+        return {
+            getRemindMeOptions: getRemindMeOptions,
+            getYesNoOptions: getYesNoOptions
+        };
+
+        function getRemindMeOptions() {
+            return [
+                {
+                    id: 'day',
+                    label: 'In a day'
+                },
+                {
+                    id: 'week',
+                    label: 'In a week'
+                },
+                {
+                    id: 'month',
+                    label: 'In a month'
+                }
+            ];
+        }
+
+        function getYesNoOptions() {
+            return [
+                {
+                    id: 'yes',
+                    label: 'Yes'
+                },
+                {
+                    id: 'no',
+                    label: 'No'
+                }
+            ];
+        }
+    }
+})();
+
